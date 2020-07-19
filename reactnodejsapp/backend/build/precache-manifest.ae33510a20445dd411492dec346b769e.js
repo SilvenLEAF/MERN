1e5a29491c7577be74c7a82870b50328748b4fdc
SilvenLEAF <SilvenLEAF@gmail.com>
2020-07-19 09:30:30 +0530

    testing to host a reactnodejs app on heroku deploying from a sub-gitdirectory

---
+++ reactnodejsapp/backend/build/precache-manifest.ae33510a20445dd411492dec346b769e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b61db8eb1278def46368c2ef2fb3e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df41531ff07119c26705",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "199f4f17d6e8b4e5c2b3",
+    "url": "./static/js/2.1345eff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1345eff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df41531ff07119c26705",
+    "url": "./static/js/main.025b9405.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
