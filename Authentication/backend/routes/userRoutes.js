801d74a3affebb0ab54f9e0ec6b14b1a47e23d07
SilvenLEAF <SilvenLEAF@gmail.com>
2020-07-25 12:50:44 +0530

    token verifying route-------------returns true and false based on whether the token is verified or not

--- Authentication/backend/routes/userRoutes.js
+++ Authentication/backend/routes/userRoutes.js
@@ -136,4 +136,34 @@ router.delete('/delete', auth, async (req, res)=>{
 
 
 
+
+
+/* --------------------------------
+.       verify token route
+-------------------------------- */
+router.post('/tokenIsValid', async (req, res)=>{
+  try {
+
+    const token = req.header('x-auth-token');  
+    if(!token)
+      return res.json(false);
+
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET); // it verifies and gives us the decoded token  
+    if(!verified)
+      return res.json(false);
+
+
+    const user = await User.findById(verified.id);
+    if(!user)
+      return res.json(false);
+
+      return res.json(true);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
+
+
 module.exports = router;
