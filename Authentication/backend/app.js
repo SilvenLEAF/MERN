b68f29dcc04cb1acf7899e8cc610c16e8b52865d
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-20 14:58:51 +0530

    kept express requiring and app firing together

--- Authentication/backend/app.js
+++ Authentication/backend/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 
 const express = require('express');
+const app= express();
+
+
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 
-// firing express app
-const app= express();
+
+
+
 
 /* -----------------------------------
 .             configuration
