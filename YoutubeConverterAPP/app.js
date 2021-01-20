54493a8191cf0957881ec7b3d537800c7c943b1c
Manash Sarma <SilvenLEAF@gmail.com>
2021-01-20 07:49:03 +0530

    Url Encoded middleware

--- YoutubeConverterAPP/app.js
+++ YoutubeConverterAPP/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlEncoded({ extended: true } ));
 app.use(express.static(path.join(__dirname, 'build')));
 
 /* --------------------------------
